23cd705c7b9660ded44e30e2742d11f86d9dd480
Dempel <dempelr@gmail.com>
2023-10-18 20:57:14 +0200

    Benötigte Klassen erstellt.

---
+++ src/org/hbrs/se1/ws23/uebung2/ConcreteMember.java
@@ -0,0 +1,14 @@
+package org.hbrs.se1.ws23.uebung2;
+
+public class ConcreteMember implements Member{
+
+
+    private int ID;
+    public ConcreteMember(int ID) {
+        this.ID = ID;
+    }
+    @Override
+    public Integer getID() {
+        return null;
+    }
+}
